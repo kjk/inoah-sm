c5788824416e15780247baebd2158c4bda713730
kjk <kjk@cb00da1d-87da-0310-af5a-c070ebdf6fc3>
2004-08-09 06:33:53 +0000

    improvements to building cab and setup files for sm
    git-svn-id: https://repo.kowalczyk.info/svn/sm_inoah/trunk@109 cb00da1d-87da-0310-af5a-c070ebdf6fc3

--- sm_inoah/build_cab.bat
+++ sm_inoah/build_cab.bat
@@ -1,8 +1,9 @@
 rmdir /q /s dist
 mkdir dist
-copy ARMRel\sm_inoah.exe dist\inoah.exe
-copy inoah.inf dist
+copy ARMRel\inoah_sm_arm_rel.exe dist\inoah.exe
+copy inoah_sm.inf dist
 cd dist
-"C:\Program Files\Windows CE Tools\wce300\Smartphone 2002\tools\CabwizSP.exe" inoah.inf
-move inoah.CAB inoah_1_0.CAB
-dir
+"C:\Program Files\Windows CE Tools\wce300\Smartphone 2002\tools\CabwizSP.exe" inoah_sm.inf
+move inoah.CAB inoah_sm_1_0.cab
+@rem ezsetup -l english -i inoah_sm.ini -r readme.txt -e eula.txt -o iNoah_Smartphone_setup.exe
+ezsetup -l english -i inoah_sm.ini -o iNoah_Smartphone_setup.exe
