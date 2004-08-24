6f8d05f4b19330558eb46db0e45d3bfe06e5bdfa
kjk <kjk@cb00da1d-87da-0310-af5a-c070ebdf6fc3>
2004-08-24 00:09:31 +0000

    updates to ppc build file
    git-svn-id: https://repo.kowalczyk.info/svn/sm_inoah/trunk@141 cb00da1d-87da-0310-af5a-c070ebdf6fc3

--- sm_inoah/build_ppc_setup.bat
+++ sm_inoah/build_ppc_setup.bat
@@ -6,8 +6,8 @@ copy eula_ppc.txt dist_ppc
 copy inoah_ppc.inf dist_ppc
 copy inoah_ppc.ini dist_ppc
 cd dist_ppc
-"C:\Program Files\Windows CE Tools\wce300\Smartphone 2002\tools\CabwizSP.exe" inoah_ppc.inf
+"C:\Program Files\Windows CE Tools\wce300\Pocket PC 2002\support\ActiveSync\windows ce application installation\cabwiz\cabwiz.exe" inoah_ppc.inf
 move inoah_ppc.CAB inoah_ppc_1_0.cab
 @rem ezsetup_sm -l english -i inoah_sm.ini -r readme.txt -e eula.txt -o iNoah_Smartphone_setup.exe
-ezsetup_sm -l english -i inoah_ppc.ini -r readme_ppc.txt -e eula_ppc.txt -o inoah_ppc_1_0_setup.exe
+ezsetup -l english -i inoah_ppc.ini -r readme_ppc.txt -e eula_ppc.txt -o inoah_ppc_1_0_setup.exe
 del *.txt *.ini *.inf inoah.exe
